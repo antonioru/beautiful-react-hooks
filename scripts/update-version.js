293e1f4bff2d058deb0aab1b4ac901cf0f1ae474
Antonio Rù <antonio@nocturnal.code>
2022-06-13 16:26:47 +0200

    fix(ci): fixes update-version script

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,1 +1,16 @@
-console.log('\n\n\n\n\n\nPUBLISHING V: ->->->', process.argv[0])
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+const nextVersion = process.argv[2]
+const pkgPath = path.join(__dirname, '..', 'package.json')
+
+const packageJsonText = fs.readFileSync(pkgPath)
+const packageJson = JSON.parse(packageJsonText)
+
+const nextPackageJson = { ...packageJson, version: nextVersion }
+
+console.log('\nUPDATING PACKAGE JSON VERSION TO: ', nextVersion)
+
+fs.writeFileSync(pkgPath, JSON.stringify(nextPackageJson))
