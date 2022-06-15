21c35341b9709adcc0722c5c32b2517712a497a1
Antonio Rù <antonio@nocturnal.code>
2022-06-15 16:32:51 +0200

    fix(useSearchQuery): fixes types for useSearchQuery

--- src/useSearchQuery.ts
+++ src/useSearchQuery.ts
@@ -4,7 +4,7 @@ import useQueryParam from './useQueryParam'
  * Ease the process of modify the 'search' query string in the URL for the current location.
  * It's just a shortcut/wrapper around useQueryParam
  */
-const useSearchQuery = <TSearchKey extends string>(initialValue: TSearchKey, replaceState = false) => useQueryParam<TSearchKey>('search', {
+const useSearchQuery = <TSearchKey extends string>(initialValue?: TSearchKey, replaceState = false) => useQueryParam<TSearchKey>('search', {
   initialValue,
   replaceState,
 })
