badfe0484a1a9ace848dc7e1c45f422032567ad7
Antonio Rù <antonio@nocturnal.code>
2022-06-15 12:29:42 +0200

    fix(exports): fixes the package.json exports file

---
+++ scripts/generate-exports.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+
+const srcPath = path.join(__dirname, '..', 'src')
+const pkgPath = path.join(__dirname, '..', 'package.json')
+
+const srcFiles = glob.sync(`${srcPath}/*.ts`)
+                     .map((file) => file.replace(`${srcPath}/`, '').replace('.ts', ''))
+                     .filter((file) => file !== 'index')
+
+const exportsObj = srcFiles.reduce((acc, file) => ({
+  ...acc, [file]: {
+    import: `./esm/${file}.js`, require: `./${file}.js`,
+  }
+}), {})
+
+const packageJsonText = fs.readFileSync(pkgPath)
+const packageJson = JSON.parse(packageJsonText)
+
+const nextPackageJson = { ...packageJson, exports: exportsObj }
+
+console.log('\nUPDATING EXPORTS: ', nextPackageJson)
+
+fs.writeFileSync(pkgPath, JSON.stringify(nextPackageJson, null, 2))
